749f2a8736b3454aa4c28e390978524c9ccfedbf
ArzuIF <arzu.findos@gmail.com>
2025-09-25 21:27:42 +0300

    Update HomeSection.tsx

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -64,18 +64,15 @@ export default function HomeSection() {
           className="hidden md:block absolute top-[10px]  right-[20px] w-auto h-auto"
         />
       </div>
-      <button 
-  className="relative bg-gradient-to-r from-[#F2C334] via-[#F2A03C] to-[#BC3964] text-white dark:text-gray-900 py-3 px-10 rounded-full shadow-lg text-sm md:text-xl font-medium transform transition-transform duration-300 ease-in-out opacity-70 cursor-not-allowed overflow-hidden group"
-  onClick={(e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  }}
->
-  <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-gray-900 dark:group-hover:border-white"></span>
-  <Link href="/eggjam" className="relative">
-    <span onClick={(e) => e.preventDefault()}>JAMINATION 8</span>
-  </Link>
-</button>
+      <div className="absolute left-1/2 transform -translate-x-1/2 top-[400px] z-10">
+        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+          <button className="relative bg-[#1a264e] dark:bg-white text-white dark:text-gray-900 py-3 px-10 rounded-full shadow-lg text-sm md:text-xl font-medium transform transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-transparent dark:hover:bg-transparent overflow-hidden group">
+            <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-gray-900 dark:group-hover:border-white"></span>
+            <a href="#about" className="relative">Keşfet</a>
+           <button className="relative bg-gradient-to-r from-[#F2C334] via-[#F2A03C] to-[#BC3964] text-white dark:text-gray-900 py-3 px-10 rounded-full shadow-lg text-sm md:text-xl font-medium transform transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-transparent dark:hover:bg-transparent overflow-hidden group">
+            <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-gray-900 dark:group-hover:border-white"></span>
+            <Link href="/eggjam" className="relative">Jamination 8 (Yakında)</Link>
+          </button>
 
         </div>
       </div>
