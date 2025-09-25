44e1d528796b37a4075fad88e70179d33ed38e4e
ArzuIF <arzu.findos@gmail.com>
2025-09-25 20:57:28 +0300

    Update HomeSection.tsx

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -70,10 +70,13 @@ export default function HomeSection() {
             <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-gray-900 dark:group-hover:border-white"></span>
             <a href="#about" className="relative">Keşfet</a>
           </button>
-          <button className="relative bg-gradient-to-r from-[#F2C334] via-[#F2A03C] to-[#BC3964] text-white dark:text-gray-900 py-3 px-10 rounded-full shadow-lg text-sm md:text-xl font-medium transform transition-transform duration-300 ease-in-out hover:scale-105 hover:bg-transparent dark:hover:bg-transparent overflow-hidden group">
-            <span className="absolute inset-0 rounded-full border-2 border-transparent group-hover:border-gray-900 dark:group-hover:border-white"></span>
-            <Link href="/eggjam" className="relative">EGG JAM</Link>
-          </button>
+          <button 
+      className="relative bg-gradient-to-r from-[#F2C334] via-[#F2A03C] to-[#BC3964] text-white opacity-50 py-3 px-10 rounded-full shadow-lg text-sm md:text-xl font-medium cursor-not-allowed"
+      disabled
+    >
+      <span className="absolute inset-0 rounded-full border-2 border-transparent"></span>
+      <span className="relative">JAMINATION 8</span>
+    </button>
         </div>
       </div>
 
